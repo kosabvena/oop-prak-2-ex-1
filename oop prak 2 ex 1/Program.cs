bec91a0e32c5d5fbed840c64660d5518b160efb7
kosabvena <kosabvena@gmail.com>
2026-09-12 21:00:18 +0200

    Add project files.

---
+++ oop prak 2 ex 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть логін: ");
+        string login = Console.ReadLine();
+
+        string pattern = @"^[A-Za-z][A-Za-z0-9]{1,9}$";
+
+        if (Regex.IsMatch(login, pattern))
+        {
+            Console.WriteLine("Логін коректний!");
+        }
+        else
+        {
+            Console.WriteLine("Логін некоректний!");
+        }
+    }
+}
